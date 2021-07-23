6b910b870c64988484bbe5f349d54686e93313f7
lcw3176 <lcw3176@naver.com>
2021-07-23 16:08:50 +0900

    테스트케이스 변경

--- dataStructure/LinkedList/test.py
+++ dataStructure/LinkedList/test.py
@@ -1,28 +1,30 @@
 from linked_list import LinkedList
 
 ls = LinkedList()
-for i in range(10):
+for i in range(100):
     ls.add(i)
 
+ls.display()
 
-for i in range(10):
+for i in range(20):
     ls.remove(0)
 
+ls.display()
 
-for i in range(10):
+for i in range(100, 120):
     ls.add(i)
 
+ls.display()
 
 print(ls.contains(10))
 print(ls.contains(2))
 print(ls.get(1))
 ls.display()
 ls.clear()
-ls.display()
 
-for i in range(10):
+for i in range(100):
     ls.add(i)
 
 print(ls.size())
 print(ls.indexOf(5))
-print(ls.indexOf(100))
+print(ls.indexOf(125))
