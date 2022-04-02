3c95ed6b4428582fa184856a7eb71e23ae03589a
joebrooks <lcw3176@naver.com>
2022-04-02 16:38:29 +0900

    RGB 거리

--- algorithm/BaekJoon/silver/1149_RGB거리.py
+++ algorithm/BaekJoon/silver/1149_RGB거리.py
@@ -1,31 +1,20 @@
 # d[1] != d[2]
 # d[n] != d[n-1]
-# d[i] != d[i - 1], d[i + 1]
+# d[i] != d[i - 1], d[i + 1], 단, i >= 2 && i <= n-1
 
-# 2 <= i <= n -1
+n = int(input())
+
+lst = []
+
+for i in range(n):
+    lst.append(list(map(int, input().split(" "))))
 
 
-# n = int(input())
-
-# lst = [0 for _ in range(n + 1)]
-# identifier = [0 for _ in range(n + 1)]
-# temp_lst = []
-
-# for i in range(0, n):
-#     temp_lst = list(map(int, input().split(" ")))
-
-# for i in temp_lst:
-#     identifier[i + 1] = temp_lst.index(int(min(temp_lst)))
-#     lst[i + 1] = min(temp_lst)
-
-#     if i >= 1 and identifier[i] == identifier[i + 1]:
-#         max_index = max(lst[i], lst[i + 1])
-#         lst[max_index]
-        
-
-#     # if i >= 2 and identifier[i + 1] == identifier[i - 1]:        
-#     #     temp_lst[identifier[i + 1]] = 100000
-#     #     identifier[i + 1] = temp_lst.index(min(temp_lst))
+for i in range(1, n):
+    lst[i][0] = min(lst[i - 1][1], lst[i - 1][2]) + lst[i][0]
+    lst[i][1] = min(lst[i - 1][0], lst[i - 1][2]) + lst[i][1]
+    lst[i][2] = min(lst[i - 1][0], lst[i - 1][1]) + lst[i][2]
+print(min(lst[n - 1]))
 
     
     
